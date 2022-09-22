ae019b09c2545c40444fe9aec6ef89b06932bb81
NathanLazo <nathanlazo@outlook.com>
2022-09-21 19:32:47 -0600

    [UPDATE] animations & spacing

--- src/components/contact/Container.js
+++ src/components/contact/Container.js
@@ -7,7 +7,7 @@ const ContactMe = () => {
     <motion.div className="flex justify-center"
     initial={{ opacity: 0, y: 50 }}
     whileInView={{ opacity: 1, y: 0 }}
-    transition={{ duration: 1 }}
+    transition={{ duration: 1, delay: 0.5 }}
     viewport={{once: true}}
     >
       <div className="bg-transparent shadow-2xl sm:rounded-lg z-50 flex h-full w-full mx-auto">
--- src/components/projects/Container.js
+++ src/components/projects/Container.js
@@ -9,7 +9,7 @@ const Projects = () => {
     className="flex flex-col"
     initial={{ opacity: 0, y: 50 }}
     whileInView={{ opacity: 1, y: 0 }}
-    transition={{ duration: 1 }}
+    transition={{ duration: 1, delay: 0.5 }}
     viewport={{once: true}}
     >
       <Title title="projects" />
